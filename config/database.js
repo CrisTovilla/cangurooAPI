f6cb16ebaf917c81337467d284935de133c1fefc
CrisTovilla <163201@ids.upchiapas.edu.mx>
2019-02-28 20:13:38 -0600

    Postgres

--- config/database.js
+++ config/database.js
@@ -74,7 +74,7 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+      host: Env.get('DB_HOST',CLEARDB_DATABASE_URL.host),
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
