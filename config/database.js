463e833639bcb32853b3a7a39c800058181f74a6
CrisTovilla <163201@ids.upchiapas.edu.mx>
2019-02-28 20:17:13 -0600

    Test

--- config/database.js
+++ config/database.js
@@ -74,7 +74,7 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST',CLEARDB_DATABASE_URL.host),
+      host: Env.get('DB_HOST','db'),
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'madnlhduyvuodk'),
       password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
