9e195ef3697e8558d4fa4c392ecc1b6ad55b0957
CrisTovilla <163201@ids.upchiapas.edu.mx>
2019-03-12 11:18:56 -0600

    database deploy

--- config/database.js
+++ config/database.js
@@ -53,10 +53,10 @@ module.exports = {
     client: 'mysql',
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      port: Env.get('DB_PORT', '3306'),
+      user: Env.get('DB_USER', 'up'),
+      password: Env.get('DB_PASSWORD', 'Up@14111989'),
+      database: Env.get('DB_DATABASE', 'canguroBD')
     },
     debug: Env.get('DB_DEBUG', false)
   },
